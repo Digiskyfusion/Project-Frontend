5d545138ef3b1e707153d51dc437a2541d8384aa
manishathakur602 <manishathakur602@gmail.com>
2025-03-01 11:04:47 +0530

    checked

--- src/Components/Channel.jsx
+++ src/Components/Channel.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import starplus from './../assets/Images/starplus.png';
+import aajtak from './../assets/Images/aajtak.png';
+
 
 
 function Channel() {
@@ -14,7 +16,20 @@ function Channel() {
                  <div>
                     <h1>STAR Plus</h1>
                    <h2>Entertainment</h2> 
+                   <p>121.3M</p>
+                   <p>5,00,000 Min Spend</p>
                  </div>
+
+                 <div>
+                 <img src={aajtak} alt='aajtak'></img>
+
+                 </div>
+
+
+
+
+
+
 
              </div>
     </div>
