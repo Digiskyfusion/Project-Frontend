89c83f955b8bf7cfb8bdbb3ea842ada069f6d9c1
priya <priyasoni6027@gmail.com>
2025-03-11 01:38:23 +0530

    Fixed build issue..

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
+          if (id.includes('node_modules')) {
+            return 'vendor';
           }
         },
       },
     },
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 1000, // Adjust limit to avoid warnings
   },
 });
