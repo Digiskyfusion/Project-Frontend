d6ac9bfb02d980c1d5c7c1972b4635f998e109df
priya <priyasoni6027@gmail.com>
2025-03-11 01:48:47 +0530

    Fixed deployment issues

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,12 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) return 'vendor';
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Move all node_modules to vendor.js
+          }
         },
       },
     },
-    chunkSizeWarningLimit: 1000, // Avoid warnings
+    chunkSizeWarningLimit: 500, // Set a lower warning limit
   },
 });
