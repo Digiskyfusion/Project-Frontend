1efd4ff1ead3aadf4910a7b7971e8e36c68119f7
priya <priyasoni6027@gmail.com>
2025-03-11 11:34:59 +0530

    Fixed Deployment issue

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: './',  // Ensures assets load correctly in deployment
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // Move dependencies to vendor.js
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 500, // Reduce bundle size warning limit
-  },
-});
+  plugins: [react(), tailwindcss(),],
+ 
+})
