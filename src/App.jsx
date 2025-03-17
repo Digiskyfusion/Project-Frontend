c7c0d3d42e342fabc639afe4f714fffdbfe1e7e1
priya <priyasoni6027@gmail.com>
2025-03-17 12:38:56 +0530

    Updated backend url..

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar"
-import Signuppage from "./Components/SignupPage/Signuppage"
-import Education from "./Components/EducationPage/Education"
-import Chat from "./Pages/Chat"
-import HomePage from "./Pages/HomePage"
-import AboutUsPage from "./Pages/AboutUsPage"
-import ContactUsPages from "./Pages/ContactUsPage"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Signuppage from "./Components/SignupPage/Signuppage";
+import Education from "./Components/EducationPage/Education";
+import Chat from "./Pages/Chat";
+import HomePage from "./Pages/HomePage";
+import AboutUsPage from "./Pages/AboutUsPage";
+import ContactUsPages from "./Pages/ContactUsPage";
 import ScrollToTop from "./Components/ScrolltopTop/ScrollToTop";
 import ChooseUSPage from "./Pages/ChooseUSPage";
 import AllFreelancerPage from "./Pages/AllFreelancerPage";
@@ -29,61 +29,33 @@ import SolarSystemPage from "./Pages/SolarSystemPage";
 import CartTvPage from "./Pages/CartTvPage";
 import PostJobPage from "./Pages/PostJobPage";
 import AllJobsPage from "./Pages/AllJobsPage";
-// import MembershipPlans from "./Components/Card";
-// import FullJOBCard from "./Components/"
-// import SquareCards from "./Components/SquareCard";
 import TermAndConditionPage from "./Pages/TermAndConditionPage";
-// import Content from "./Components/Content";
-//  import Work from "./Components/Work";
-// import PrivacyPolicy from "./Components/PrivacyPolicy/PrivacyNdPolicy";
-// import CancellationNdRefund from "./Components/CancellationRefund/CancellationNdRefund";
 import PrivacyNdPolicyPage from "./Pages/PrivacyNdPolicyPage";
 import CancellationRefundPage from "./Pages/CancellationRefundPage";
 
-
-
-
-
 function App() {
-  
-
   return (
-    <>
-
-<BrowserRouter>
-<ScrollToTop /> 
-<Navbar />   
-  <Router>  
+    <BrowserRouter>
+      <ScrollToTop />
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/MembershipPlans" element={<MembershipPlans />} /> */}
         <Route path="/aboutus" element={<AboutUsPage />} />
         <Route path="/ChooseUSPage" element={<ChooseUSPage />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/allfreelancer" element={<AllFreelancerPage />} />
-        <Route path="/service" element={ <Services />} /> 
-        <Route path="/dashboard" element={ <DashboardPage />} /> 
-        <Route path="/FreelancerNoUpadte" element={  <FreelancerProfilePage />} />
-        <Route path="/FreelancreClientPage" element={ <FreelancreClientPage />} />
-        <Route path="/FreelancerUpadte" element={  <FreelancerDetails />} />
-        <Route path="/ClientForm" element={ <ClientFormPage />} />
+        <Route path="/service" element={<Services />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/FreelancerNoUpadte" element={<FreelancerProfilePage />} />
+        <Route path="/FreelancreClientPage" element={<FreelancreClientPage />} />
+        <Route path="/FreelancerUpadte" element={<FreelancerDetails />} />
+        <Route path="/ClientForm" element={<ClientFormPage />} />
         <Route path="/contactus" element={<ContactUsPages />} />
         <Route path="/registration" element={<Signuppage />} />
         <Route path="/Subcatagory" element={<SubCategoryPage />} />
-        <Route path="/service" element={ <Services />} />
-        <Route path="/dashboard" element={ <DashboardPage />} /> 
-        <Route path="/FreelancerNoUpadte" element={   <FreelancerProfilePage />} />
-        <Route path="/FreelancreClientPage" element={    <FreelancreClientPage />} />
-        <Route path="/FreelancerUpadte" element={    <FreelancerDetails />} />
-        <Route path="/ClientForm" element={ <ClientFormPage />} />
-        <Route path="/contactus" element={<ContactUsPages />} />
-        <Route path="/registration" element={<Signuppage />} />
-        <Route path="/Subcatagory" element={<SubCategoryPage  />} />
-        <Route path="/freelancerDetails" element={<Education  />} />
-        <Route path="/clientDetails" element={<CardsProfile  />} />
-        <Route path="/login" element={<Loginform  />} />
-        {/* <Route path="/discover" element={<DiscoverHire   />} /> */}
-        {/* <Route path="/client" element={<Clientformmpage />} /> */}
+        <Route path="/freelancerDetails" element={<Education />} />
+        <Route path="/clientDetails" element={<CardsProfile />} />
+        <Route path="/login" element={<Loginform />} />
         <Route path="/ClientProfile" element={<ClientProfile />} />
         <Route path="/ADCDPAGE" element={<StarPlusPage />} />
         <Route path="/channel" element={<TvAdvertisingPage />} />
@@ -91,32 +63,16 @@ function App() {
         <Route path="/ajtak" element={<AjTakPage />} />
         <Route path="/colors" element={<ColorPages />} />
         <Route path="/astha" element={<AsthaPage />} />
-
-        {/* <Route path="/detailpage" element={<DetailPage />} /> */}
-
         <Route path="/solarsystem" element={<SolarSystemPage />} />
         <Route path="/cart" element={<CartTvPage />} />
         <Route path="/postjob" element={<PostJobPage />} />
         <Route path="/all-jobs" element={<AllJobsPage />} />
-        {/* <Route path="/card" element={<MembershipPlans />} /> */}
-        {/* <Route path="/FullJobCard" element={<FullJOBCard />} /> */}
-        {/* <Route path="/SquareCards" element={<SquareCards />} /> */}
         <Route path="/TermCondition" element={<TermAndConditionPage />} />
-        {/* <Route path="/Content" element={<Content />} /> */}
-        {/* <Route path="/Work" element={<Work />} /> */}
         <Route path="/PrivacyNdPolicy" element={<PrivacyNdPolicyPage />} />
-        {/* <Route path="/CancellationNdRefund" element={<CancellationRefundPage />} /> */}
-
+        <Route path="/CancellationNdRefund" element={<CancellationRefundPage />} />
       </Routes>
-      </Router>
     </BrowserRouter>
-
-   
-    
-
-  
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
