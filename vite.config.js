cd2f58398796c384a8d3257274a9365fe74f9d51
priya <priyasoni6027@gmail.com>
2025-03-19 00:14:50 +0530

    fix index.html redirection..

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/", // Ensures proper path resolution
+  server: {
+    historyApiFallback: true, // Enables routing fallback for React Router
+  },
 });
-
