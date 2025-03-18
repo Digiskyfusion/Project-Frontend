8b26f27570214286a3f71314cb9285c2f58d020b
priya <priyasoni6027@gmail.com>
2025-03-18 23:53:19 +0530

    fix index.html redirection..

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./", // Ensures relative paths are used
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
-  build: {
-    outDir: "dist",
-  },
-  preview: {
-    port: 4173,
-  },
+  base: "/", // Ensures proper path resolution
 });
+
