492d3bf7b04218352cc25eab677273cd40042e9b
priya <priyasoni6027@gmail.com>
2025-03-22 23:11:02 +0530

    Git pull from manish branch

--- src/Components/SignupPage/Signuppage.jsx
+++ src/Components/SignupPage/Signuppage.jsx
@@ -38,7 +38,7 @@ function Signuppage() {
     }
     try {
       // const response = await axios.post(`${API_URL}/auth/register`, formData);
-      const response = await axios.post(`http://localhost:3000/api/auth/signup`, formData);
+      const response = await axios.post(`${API_URL}/api/auth/signup`, formData);
       console.log("response");
       console.log(response);
       toast.success("Signup successful!");
