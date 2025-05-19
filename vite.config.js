48876aca57eaf0733de2eed93a347e870d01292f
bhavya-bansal-29 <bhavyabansal1357@gmail.com>
2025-05-19 17:33:37 +0530

    Vercel deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()], //remove tailwind css and used as postcss import
   base: "/", // Ensures proper path resolution
   server: {
     historyApiFallback: true, // Enables routing fallback for React Router
