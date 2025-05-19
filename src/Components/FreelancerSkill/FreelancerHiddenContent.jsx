ccb55b84fea007ffd6e4e2e7c7db7c6825e9af3e
Manish <139942794+Manish6906@users.noreply.github.com>
2025-05-19 14:37:07 +0530

    final commit

---
+++ src/Components/FreelancerSkill/FreelancerHiddenContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FreelancerHiddenContent() {
+  return (
+    <div className='hidden'>
+       <p>If you are not redirected automatically, <a href="https://digisky.ai">click here</a>.</p>
+
+    </div>
+  )
+}
+
+export default FreelancerHiddenContent
--- src/Components/SignupPage/Signuppage.jsx
+++ src/Components/SignupPage/Signuppage.jsx
@@ -23,6 +23,8 @@ function Signuppage() {
     roleType: "",
     state: "",
     mobileNumber: "",
+    city:"",
+    country:"",
   });
 
   const handleChange = (e) => {
@@ -104,6 +106,59 @@ function Signuppage() {
             </label>
             <label className="relative w-full">
   <select
+    name="country"
+    value={formData.country}
+    onChange={handleChange}
+    className="w-full p-2 border rounded-lg appearance-none bg-white"
+  >
+    <option value="">Select a country</option>
+    <option value="Andhra Pradesh">Andhra Pradesh</option>
+    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+    <option value="Assam">Assam</option>
+    <option value="Bihar">Bihar</option>
+    <option value="Chhattisgarh">Chhattisgarh</option>
+    <option value="Goa">Goa</option>
+    <option value="Gujarat">Gujarat</option>
+    <option value="Haryana">Haryana</option>
+    <option value="Himachal Pradesh">Himachal Pradesh</option>
+    <option value="Jharkhand">Jharkhand</option>
+    <option value="Karnataka">Karnataka</option>
+    <option value="Kerala">Kerala</option>
+    <option value="Madhya Pradesh">Madhya Pradesh</option>
+    <option value="Maharashtra">Maharashtra</option>
+    <option value="Manipur">Manipur</option>
+    <option value="Meghalaya">Meghalaya</option>
+    <option value="Mizoram">Mizoram</option>
+    <option value="Nagaland">Nagaland</option>
+    <option value="Odisha">Odisha</option>
+    <option value="Punjab">Punjab</option>
+    <option value="Rajasthan">Rajasthan</option>
+    <option value="Sikkim">Sikkim</option>
+    <option value="Tamil Nadu">Tamil Nadu</option>
+    <option value="Telangana">Telangana</option>
+    <option value="Tripura">Tripura</option>
+    <option value="Uttar Pradesh">Uttar Pradesh</option>
+    <option value="Uttarakhand">Uttarakhand</option>
+    <option value="West Bengal">West Bengal</option>
+    <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+    <option value="Chandigarh">Chandigarh</option>
+    <option value="Dadra and Nagar Haveli and Daman and Diu">Dadra and Nagar Haveli and Daman and Diu</option>
+    <option value="Lakshadweep">Lakshadweep</option>
+    <option value="Delhi">Delhi</option>
+    <option value="Puducherry">Puducherry</option>
+    <option value="Ladakh">Ladakh</option>
+    <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+  </select>
+</label>
+
+          </div>
+
+
+
+
+          {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+   <label className="relative w-full">
+  <select
     name="state"
     value={formData.state}
     onChange={handleChange}
@@ -149,7 +204,54 @@ function Signuppage() {
   </select>
 </label>
 
-          </div>
+<label className="relative w-full">
+  <select
+    name="city"
+    value={formData.city}
+    onChange={handleChange}
+    className="w-full p-2 border rounded-lg appearance-none bg-white"
+  >
+    <option value="">Select a city</option>
+    <option value="Andhra Pradesh">Andhra Pradesh</option>
+    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+    <option value="Assam">Assam</option>
+    <option value="Bihar">Bihar</option>
+    <option value="Chhattisgarh">Chhattisgarh</option>
+    <option value="Goa">Goa</option>
+    <option value="Gujarat">Gujarat</option>
+    <option value="Haryana">Haryana</option>
+    <option value="Himachal Pradesh">Himachal Pradesh</option>
+    <option value="Jharkhand">Jharkhand</option>
+    <option value="Karnataka">Karnataka</option>
+    <option value="Kerala">Kerala</option>
+    <option value="Madhya Pradesh">Madhya Pradesh</option>
+    <option value="Maharashtra">Maharashtra</option>
+    <option value="Manipur">Manipur</option>
+    <option value="Meghalaya">Meghalaya</option>
+    <option value="Mizoram">Mizoram</option>
+    <option value="Nagaland">Nagaland</option>
+    <option value="Odisha">Odisha</option>
+    <option value="Punjab">Punjab</option>
+    <option value="Rajasthan">Rajasthan</option>
+    <option value="Sikkim">Sikkim</option>
+    <option value="Tamil Nadu">Tamil Nadu</option>
+    <option value="Telangana">Telangana</option>
+    <option value="Tripura">Tripura</option>
+    <option value="Uttar Pradesh">Uttar Pradesh</option>
+    <option value="Uttarakhand">Uttarakhand</option>
+    <option value="West Bengal">West Bengal</option>
+    <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+    <option value="Chandigarh">Chandigarh</option>
+    <option value="Dadra and Nagar Haveli and Daman and Diu">Dadra and Nagar Haveli and Daman and Diu</option>
+    <option value="Lakshadweep">Lakshadweep</option>
+    <option value="Delhi">Delhi</option>
+    <option value="Puducherry">Puducherry</option>
+    <option value="Ladakh">Ladakh</option>
+    <option value="Jammu and Kashmir">Jammu and Kashmir</option>
+  </select>
+</label>
+
+</div> */}
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
             <label className="relative">
