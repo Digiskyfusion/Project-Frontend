0e40aa057cf59130dfccc094fd90d193ebb445a5
react <reactjs@techquadra.com>
2025-09-05 18:31:52 +0530

    >

--- src/Components/ForgetPassword/ForgetPassword.jsx
+++ src/Components/ForgetPassword/ForgetPassword.jsx
@@ -9,7 +9,7 @@ const ForgotPassword = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`http://localhost:5000/api/forget-password`, {
+      const response = await fetch(`${API_URL}/api/forget-password`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
